ada8ece72213cb52bbeea24c9a2e00e099b03397
dyc1lyf <dyc1lyf@163.com>
2021-04-30 18:08:00 +0800

    123

--- buildScript.sh
+++ buildScript.sh
@@ -1,5 +1,19 @@
-# 确保脚本抛出遇到的错误
-set -e
+# 编译脚本 buildScript.sh
 
-# 生成静态文件
+# 更新依赖
+
+logDate=$(date +"%Y-%m-%d-%H-%M")
+
+echo "更新依赖"
+
+npm install
+
+echo "编译项目"
+
 npm run build
+
+echo "设置权限"
+
+chown -R www:www dist/[^.]*
+
+echo "运行结束"
