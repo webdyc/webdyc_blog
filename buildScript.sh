67b7ced1f3a976d19b88e54d9678438c27f9ebde
dyc1lyf <dyc1lyf@163.com>
2021-04-30 16:52:53 +0800

    1

--- buildScript.sh
+++ buildScript.sh
@@ -1,35 +1,5 @@
-# 编译脚本 buildScript.sh
+# 确保脚本抛出遇到的错误
+set -e
 
-# logUpdatePath="buildScript.sh"
-
-# logBuildPath="docs/.vuepress/dist"
-
-# 更新依赖
-echo "进入脚本"
-logDate=$(date +"%Y-%m-%d-%H-%M")
-
-# if [ ! -d "$logUpdatePath" ]; then
-
-# mkdir -p $logUpdatePath
-
-# fi
-
-# if [ ! -d "$logBuildPath" ]; then
-
-# mkdir -p $logBuildPath
-
-# fi
-
-echo "更新依赖"
-
-npm install
-
-echo "编译项目"
-
+# 生成静态文件
 npm run build
-
-echo "设置权限"
-
-chown -R www:www docs/.vuepress/dist/[^.]*
-
-echo "运行结束"
