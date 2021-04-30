41022e3ee977a8cd876a253d77b545d261f0e4ad
dyc1lyf <dyc1lyf@163.com>
2021-04-30 15:18:34 +0800

    11

--- buildScript.sh
+++ buildScript.sh
@@ -30,9 +30,6 @@ npm run build
 
 echo "设置权限"
 
-chown -R www:www dist/[^.]*
+chown -R www:www docs/.vuepress/dist/[^.]*
 
 echo "运行结束"
-————————————————
-版权声明：本文为CSDN博主「weixin_39951295」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/weixin_39951295/article/details/111857257
